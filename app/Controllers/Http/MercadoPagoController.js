432f61bb4bd685d3746653f9fb2cf8385ab4adca
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-12-09 13:23:11 -0300

    Fix mercadopago returns

--- app/Controllers/Http/MercadoPagoController.js
+++ app/Controllers/Http/MercadoPagoController.js
@@ -14,23 +14,36 @@ class MercadoPagoController {
 
         let items = await Product
             .query()
+            .with('category')
             .whereIn('id', items_id)
             .fetch()
 
         items = items.toJSON()
         items = items.map((item) => {
             return {
+                id: `#${item.id} ${item.title}`,
                 title: item.title,
                 unit_price: Number(item.money),
+                category_id: item.category.name,
+                picture_url: item.image_large,
+                currency_id: "BRL",
                 quantity: 1,
             }
         })
 
         console.log(items)
 
-        const response = await mercadopago.preferences.create({items})
+        const response = await mercadopago.preferences.create({
+            items,
+            back_urls: {
+                success: `${process.env.FRONT_END}/paid`,
+                failure: `${process.env.FRONT_END}/paid`,
+                pending: `${process.env.FRONT_END}/paid`
+            },
+            auto_return: "approved"
+        })
 
-        return response.body.id
+        return response.body
     }
 
 }
